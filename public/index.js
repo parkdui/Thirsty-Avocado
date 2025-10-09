a8329f2816e551b966a03f30b9f4b2ac378fe100
Jiwon Park <parkgiwon050@gmail.com>
2025-10-10 03:48:05 +0900

    Feat: updated design index.html

--- public/index.js
+++
@@ -1,5 +0,0 @@
-import { Application } from "@splinetool/runtime";
-
-const canvas = document.getElementById("canvas3d");
-const app = new Application(canvas);
-app.load("https://prod.spline.design/x7tijJe3V0-ecl0S/scene.splinecode");
